cbe60495027268a4ff3a620e85842f9cce646033
Sazzad Alam Bhuiyan <60057234+Sazzad05@users.noreply.github.com>
2021-02-01 21:44:50 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,25 +2,28 @@ console.log("CGPA Calculator Made By Sazzad Alam Bhuiyan.");
 
 let output = document.getElementById("outputCGPA");
 let outputC = document.getElementById("outputCredit");
-let btn = document.querySelector(".btn");
 
-console.log(btn);
+
+
 
 function calcunation(ce, cgpa, c, gpa) {
   return (((ce * cgpa) + (c * gpa)) / (ce * 1 + 1 * c));
 }
 
-function addCourse(ce, cgpa, c, newcgpa) {
-  ce = (ce * 1 + 1 * c);
-  cgpa = newcgpa.toFixed(2);
-  return [ce, cgpa];
+
+function removeCourse(ce, cgpa, c, gpa) {
+  return (((ce * cgpa) - (c * gpa)) / (ce * 1 - 1 * c));
 }
+
 var creditEarned = document.querySelector(".CreditEarned");
 var currnetCGPA = document.querySelector(".CurrnetCGPA");
 var courseCredit = document.querySelector(".CourseCredit");
 var earnedGPA = document.querySelector(".EarnedGPA");
-var print;
+var retakeCredit = document.querySelector(".retakecredit");
+var previousGPA = document.querySelector(".oldgpa");
+var updatedGPA = document.querySelector(".newgpa");
 var ans;
+var credit;
 
 function liveInput() {
   console.log(creditEarned.value);
@@ -32,9 +35,9 @@ function liveInput() {
   if (creditEarned.value <= 999 && courseCredit.value <= 99) {
     if (currnetCGPA.value >= 0 && earnedGPA.value >= 0 && currnetCGPA.value <= 4 && earnedGPA.value <= 4) {
       ans = calcunation(creditEarned.value, currnetCGPA.value, courseCredit.value, earnedGPA.value);
-      print = parseFloat(ans) || 0;
-      output.innerText = print.toFixed(2);
-      outputC.innerText = (creditEarned.value * 1 + 1 * courseCredit.value).toFixed(1);
+      output.innerText = ans.toFixed(2);
+      credit = (creditEarned.value * 1 + 1 * courseCredit.value);
+      outputC.innerText = credit.toFixed(1);
     } else {
       output.innerHTML = "<h4>Invalid</h4>";
     }
@@ -43,15 +46,50 @@ function liveInput() {
   }
 }
 
-btn.addEventListener("click", function() {
-  let updated = addCourse(creditEarned.value, currnetCGPA.value, courseCredit.value, ans);
-  console.log(ans)
-  creditEarned.value = updated[0];
+
+function updateInputBox() {
+  creditEarned.value = (creditEarned.value * 1 + 1 * courseCredit.value);
+  currnetCGPA.value = ans;
+  courseCredit.value = '';
+  earnedGPA.value = '';
+}
+
+function addRetake() {
+  creditEarned.value = (creditEarned.value * 1 + 1 * courseCredit.value);
   currnetCGPA.value = ans;
   courseCredit.value = '';
   earnedGPA.value = '';
 
-  console.log("updated: " + creditEarned.value)
-  console.log("updated: " + currnetCGPA.value)
-
-})
+  if (retakeCredit.value * 1 <= creditEarned.value * 1 &&
+    previousGPA.value * 1 <= currnetCGPA.value * 1 &&
+    retakeCredit.value * 1 <= 99 &&
+    retakeCredit.value * 1 >= 0 &&
+    previousGPA.value * 1 <= 4 &&
+    previousGPA.value * 1 >= 0 &&
+    updatedGPA.value * 1 >= 0 &&
+    updatedGPA.value * 1 <= 4) {
+    console.log(previousGPA.value);
+    console.log(updatedGPA.value);
+    let temp1 = removeCourse(credit, ans, retakeCredit.value, previousGPA.value);
+    let temp2 = (credit * 1 - 1 * retakeCredit.value);
+    console.log(currnetCGPA.value);
+    // creditEarned.value = credit;
+    ans = calcunation(temp2, temp1, retakeCredit.value, updatedGPA.value);
+    currnetCGPA.value = ans;
+    creditEarned.value = credit;
+    output.innerText = ans.toFixed(2);
+    outputC.innerText = credit.toFixed(1);
+    courseCredit.value = '';
+    earnedGPA.value = '';
+    retakeCredit.value = '';
+    previousGPA.value = '';
+    updatedGPA.value = '';
+  } else {
+    alert("Input error. Make sure to input correct value");
+    courseCredit.value = '';
+    earnedGPA.value = '';
+    retakeCredit.value = '';
+    previousGPA.value = '';
+    updatedGPA.value = '';
+  }
+}
